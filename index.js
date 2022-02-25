695061aa4374c76993fb12feb4ab05e0080f47a6
DIMK1ng <davidmezzi10@gmail.com>
2022-02-25 19:22:50 +0100

    examenjs

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-function _id(ident){
+function _id(ident) {
     return document.getElementById(ident);
 }
 
-function factorial(){
-    
+function factorial() {
+    var entrada;
+    var resultat;
+    for (var i = 1; i <= document.getElementById('n').value; i++) {
+        factorial *= i;
+    }
+    document.getElementById('factorial').value = factorial;
+    _id("resultatFactorial") = innerHTML
 }
 
-function calculaIVA(){
-    
+function calculaIVA() {
+    var preuAmbIVA = $("input[name=preuAmbIVA]").val();
+    var IVA = (preuAmbIVA * tasa) / 100;
+    $("input[name=total]").val(parseInt(preuAmbIVA) + parseInt(iva));
 }
 
-function canviaFons(tipus){
-    
+function canviaFons(tipus) {
+    var colorFondo;
+    var colorTexto;
+    if (tipus == "random") {
+        var red = Math.floor(Math.random() * 255);
+        colorFondo = "rgb(" + red + "," + red + "," + red + ")";
+    }
+    else if (tipus == "clear") {
+        colorFondo = "white";
+        colorTexto = "black";
+    }
+    _id("cuerpo").style.backgroundColor = colorFondo;
+    _id("cuerpo").style.color = colorTexto;
 }
